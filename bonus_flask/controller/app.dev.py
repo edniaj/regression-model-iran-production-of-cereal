1a7be80b9d6c80dc6245fcfecf03bf8c3160dd45
edniaj <jiande_goh@mymail.sutd.edu.sg>
2023-11-24 15:57:24 +0800

    Deleted react since we pivoted to flask. Edited readme.md

--- bonus_flask/controller/app.dev.py
+++ bonus_flask/controller/app.dev.py
@@ -1,29 +1,106 @@
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, html, dash_table, dcc
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+from dash.dependencies import Input, Output
+import plotly.express as px
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+import pandas as pd
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 
-# Use the following function when accessing the value of 'my-slider'
-# in callbacks to transform the output value to logarithmic
-def transform_value(value):
-    return 10 ** value
+'''
+This file serves as a documentation / tutorial for other people to maintain yet unfamiliar with dash library
+'''
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-app.layout = html.Div([
-    dcc.Slider(0, 3, 0.01,
-        id='slider-updatemode',
-        marks={i: '{}'.format(10 ** i) for i in range(4)},
-        value=2,
-        updatemode='drag'
-    ),
-    html.Div(id='updatemode-output-container', style={'marginTop': 20})
-])
+# Initialize the app
+app = Dash(__name__)
 
-@callback(Output('updatemode-output-container', 'children'),
-              Input('slider-updatemode', 'value'))
-def display_value(value):
-    return 'Linear Value: {} | \
-            Log Value: {:0.2f}'.format(value, transform_value(value))
+# # App layout
+# app.layout = html.Div([
+#     html.Div(children='My First App with Data and a Graph'),
+#     dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    
+#     #instantiate histogram from plotly express
+#     # x is column, y is also column name, averages is aggergate function
+#     dcc.Graph(figure=px.histogram(df, x='continent', y='lifeExp', histfunc='avg'))
+# ])
 
+# App layout this includes control and callbacks
+# app.layout = html.Div([
+    
+#     html.Div(children='My First App with Data, Graph, and Controls'),
+#     html.Hr(),
+    
+#     #dcc are control components - radio button is controlling the barchart
+#     dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'], value='lifeExp', id='controls-and-radio-item'),
+    
+    
+#     dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+    
+#     # Raio item is controlling this
+#     dcc.Graph(figure={}, id='controls-and-graph')
+# ])
+
+# # Add controls to build the interaction
+# @callback(
+#     #Inputs into here, it passes to the dcc.graph  (matches  the id of the dcc graph)
+#     Output(component_id='controls-and-graph', component_property='figure'),
+    
+#     #layout actually passes value to this input (look at the component_id matches the id of the dcc. Value extracted from)
+#     Input(component_id='controls-and-radio-item', component_property='value')
+# )
+# def update_graph(col_chosen):
+#     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+#     return fig #right now dcc.Graph(figure=fig)
+
+# # Input connected to dcc.RadioItems. component_property='value' -> takes the VALUE out of dcc
+# # Output connected to dcc.Graph  -> component_proprety='figure' -> give figure to dcc.Graph -> update_graph is called
+
+# # How is update_graph connected? callback is a decorator for the update_graph function. update_graph looks for Output component_id and then interacts with that using the Input['component_property']
+
+
+## Styling your app #stylomilo
+
+# # Incorporate data
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# # Initialize the app - incorporate css
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+# # App layout
+# app.layout = html.Div([
+#     html.Div(className='row', children='My First App with Data, Graph, and Controls',
+#              style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+
+#     html.Div(className='row', children=[
+#         dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'],
+#                        value='lifeExp',
+#                        inline=True,
+#                        id='my-radio-buttons-final')
+#     ]),
+
+#     html.Div(className='row', children=[
+#         html.Div(className='six columns', children=[
+#             dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
+#         ]),
+#         html.Div(className='six columns', children=[
+#             dcc.Graph(figure={}, id='histo-chart-final')
+#         ])
+#     ])
+# ])
+
+# # Add controls to build the interaction
+# @callback(
+#     Output(component_id='histo-chart-final', component_property='figure'),
+#     Input(component_id='my-radio-buttons-final', component_property='value')
+# )
+# def update_graph(col_chosen):
+#     fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+#     return fig
+
+
+
+# Run the app
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ bonus_flask/controller/readme.md
@@ -0,0 +1,48 @@
+# Project Installation
+
+
+![Alt text](image.png)
+
+Installation
+
+## For Windows:
+
+1. Change to the `controller` directory:
+cd controller
+
+2. Create a virtual environment named `myenv`:
+python -m venv myenv
+
+3. Activate the virtual environment:
+.\myenv\Scripts\Activate
+
+4. Install the required packages from `requirements.txt`:
+pip install -r requirements.txt
+
+
+## For Linux/MacOS:
+
+1. Change to the `controller` directory:
+cd controller
+
+
+2. Create a virtual environment named `venv`:
+python -m venv venv
+
+3. Activate the virtual environment:
+source venv/bin/activate
+
+
+4. Install the required packages from `requirements.txt`:
+pip install -r requirements.txt
+
+
+
+# Acknowledgement
+
+- [FAO - Agriculture Land Data](https://www.fao.org/faostat/en/#country/102)
+- [World Bank - Climate Data (Rainfall and Temperature)](https://climateknowledgeportal.worldbank.org/country/iran-islamic-rep/climate-data-historical)
+- [UN Population Data](https://data.un.org/Data.aspx?q=iran&d=PopDiv&f=variableID%3a12%3bcrID%3a364)
+- [IMF - Private Debt Data](https://www.imf.org/external/datamapper/profile/IRN)
+- [IMF - Unemployment Data](https://www.imf.org/en/Publications/WEO/weo-database/2023/October/download-entire-database)
+- [Macrotrends - Iran Economic Growth Rate Data](https://www.macrotrends.net/countries/IRN/iran/economic-growth-rate) (Manually transcribed onto a CSV file due to the lack of a download button)
